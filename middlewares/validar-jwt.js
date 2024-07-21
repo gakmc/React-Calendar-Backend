cb0678cae5754def1ea8540cc0e1b3b8e3229a7a
Gabriel <gak335@gmail.com>
2024-07-21 16:28:04 -0400

    correccion a validar-jwt returnres

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -23,7 +23,7 @@ const validarJWT = (req, res = response, next)=>{
         req.uid = uid;
         req.name = name;
     } catch (error) {
-        returnres.status(401).json({
+        return res.status(401).json({
             ok:false,
             msg: 'Token no valido'
         })
